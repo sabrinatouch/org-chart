7868919cee3250d6a7ca7728967d5f656ba0b4d4
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 14:44:47 -0600

    Rendered a status bar

--- org-chart/src/components/OrgChart/OrgChart.js
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -5,10 +5,38 @@ import './OrgChart.css';
 import EmployeeCard from '../EmployeeCard/EmployeeCard';
 import { data } from '../../data/data.js';
 
+const StatusBar = ({status}) => {
+    const statusBarStyle = {
+        height: 20,
+        width: '100%',
+        backgroundColor: "#ffffff",
+    }
+
+    // // Conditional Rendering ?
+    // const statusColor = ({status}) => {
+    //     switch (status) {
+    //         case "normal":
+    //             return "#4caf50";
+    //             break;
+    //         case "warning":
+    //             return "#ffeb3b";
+    //             break;
+    //         case "critical":
+    //             return "#f44336";
+    //             break;
+    //     }
+    // };
+
+    return (
+        <div style={statusBarStyle} />
+    );
+}
+
 const Employee = ({employee}) => {
     return (
         <div class="container-style">
-            {employee.status + " " + employee.position + " " + employee.location}
+            <StatusBar />
+            {employee.position + " " + employee.location}
         </div>
     );
 };
