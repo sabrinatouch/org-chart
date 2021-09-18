97b33965a6c96c0e912be3411d8a38443ee395f3
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 09:54:28 -0600

    Initialized components files

---
+++ org-chart/src/components/EmployeeCard/EmployeeCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EmployeeCard extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default EmployeeCard;
---
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class OrgChart extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default OrgChart;
