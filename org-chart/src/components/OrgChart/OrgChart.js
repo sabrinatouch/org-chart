6645a17668392ef46df4b7a28a384ec3ce179951
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 15:14:22 -0600

    Colored status bar based on status

--- org-chart/src/components/OrgChart/OrgChart.js
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -5,38 +5,42 @@ import './OrgChart.css';
 import EmployeeCard from '../EmployeeCard/EmployeeCard';
 import { data } from '../../data/data.js';
 
-const StatusBar = ({status}) => {
+const StatusBarColor = ({color}) => {
     const statusBarStyle = {
         height: 20,
         width: '100%',
-        backgroundColor: "#ffffff",
+        backgroundColor: color
     }
-
-    // // Conditional Rendering ?
-    // const statusColor = ({status}) => {
-    //     switch (status) {
-    //         case "normal":
-    //             return "#4caf50";
-    //             break;
-    //         case "warning":
-    //             return "#ffeb3b";
-    //             break;
-    //         case "critical":
-    //             return "#f44336";
-    //             break;
-    //     }
-    // };
 
     return (
         <div style={statusBarStyle} />
     );
 }
 
+// Conditional Rendering ?
+const StatusBar = ({status}) => {
+    switch (status) {
+        case "normal":
+            return <StatusBarColor color={"#4caf50"} />;
+            break;
+        case "warning":
+            return <StatusBarColor color={"#ffeb3b"} />;
+            break;
+        case "critical":
+            return <StatusBarColor color={"#f44336"} />;
+            break;
+    }
+};
+
 const Employee = ({employee}) => {
     return (
         <div class="container-style">
-            <StatusBar />
-            {employee.position + " " + employee.location}
+            <StatusBar status={employee.status} />
+            {/* <StatusBar color={} /> */}
+            <div>
+                {employee.position} <br />
+                {employee.location}
+            </div>
         </div>
     );
 };
