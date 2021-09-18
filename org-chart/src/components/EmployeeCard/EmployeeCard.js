08f8462927efb16e95355791a76203449469f2e6
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 17:34:49 -0600

    Added CSS to change display of cards

--- org-chart/src/components/EmployeeCard/EmployeeCard.js
+++ org-chart/src/components/EmployeeCard/EmployeeCard.js
@@ -2,68 +2,49 @@ import React, { useState } from 'react';
 import './EmployeeCard.css';
 import OrgChart from '../OrgChart/OrgChart';
 
-// const EmployeeCard = ({node}) => {
-//     const [childVisible, setChildVisibility] = useState(false);
-
-//     // Does the current tree node have children (true) or not (false)
-//     const hasChild = node.children ? true : false
-
-//     // const hasChild = () => {
-//     //     if (node.children) {
-//     //         return true;
-//     //     } else {
-//     //         return false;
-//     //     }
-//     // }
-
-//     const StatusBarColor = ({color}) => {
-//         const statusBarStyle = {
-//             height: 20,
-//             width: '100%',
-//             backgroundColor: color
-//         }
+const EmployeeCard = ({node}) => {
+    const StatusBarColor = ({color}) => {
+        const statusBarStyle = {
+            height: 20,
+            width: '100%',
+            backgroundColor: color
+        }
     
-//         return (
-//             <div style={statusBarStyle} />
-//         );
-//     }
+        return (
+            <div style={statusBarStyle} />
+        );
+    }
     
-//     // Conditional Rendering ?
-//     const StatusBar = ({status}) => {
-//         switch (status) {
-//             case "normal":
-//                 return <StatusBarColor color={"#4caf50"} />;
-//                 break;
-//             case "warning":
-//                 return <StatusBarColor color={"#ffeb3b"} />;
-//                 break;
-//             case "critical":
-//                 return <StatusBarColor color={"#f44336"} />;
-//                 break;
-//         }
-//     };
-//     // Render current tree node 
-//     return (
-//         <div>
-//             <div class="container-style">
-//                 {
-//                     hasChild && 
-//                     <div>
-//                         <StatusBar status={node.status} />
-//                         {node.position} <br />
-//                         {node.location}
-//                     </div>
-//                 }
-//             </div>
+    // Conditional Rendering ?
+    const StatusBar = ({status}) => {
+        switch (status) {
+            case "normal":
+                return <StatusBarColor color={"#4caf50"} />;
+                break;
+            case "warning":
+                return <StatusBarColor color={"#ffeb3b"} />;
+                break;
+            case "critical":
+                return <StatusBarColor color={"#f44336"} />;
+                break;
+        }
+    };
+    // Render current tree node 
+    return (
+        <div class="chart-style">
+            <div class="container-style">
+                <div>
+                    <StatusBar status={node.status} />
+                    {node.position} <br />
+                    {node.location}
+                </div>
+            </div>
 
-//             {
-//                 hasChild && 
-//                 <ul>
-//                     <OrgChart data={node.children} />
-//                 </ul>
-//             }
-//         </div>
-//     );
-// };
+            <div class="container">
+                <OrgChart data={node.children} />
+            </div>
+        </div>
+    );
+};
 
-// export default EmployeeCard;
+export default EmployeeCard;
--- org-chart/src/components/OrgChart/OrgChart.js
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -2,73 +2,17 @@
 
 import React, { Component } from 'react';
 import './OrgChart.css';
-// import EmployeeCard from '../EmployeeCard/EmployeeCard';
+import EmployeeCard from '../EmployeeCard/EmployeeCard';
 
 const OrgChart = ({ data = [] }) => {
     // Map initial read in data to be the root of the tree
     return (
         <div>
-            {data.map(employee => {
-                console.log("hello");
+            {data.map((employee) => {
                 return (
                     <EmployeeCard node={employee} />
                 );
             })}
-        </div>
-    );
-};
-
-const EmployeeCard = ({node}) => {
-
-    // Does the current tree node have children (true) or not (false)
-    const hasChild = node.children ? true : false
-
-    // const hasChild = () => {
-    //     if (node.children) {
-    //         return true;
-    //     } else {
-    //         return false;
-    //     }
-    // }
-
-    const StatusBarColor = ({color}) => {
-        const statusBarStyle = {
-            height: 20,
-            width: '100%',
-            backgroundColor: color
-        }
-    
-        return (
-            <div style={statusBarStyle} />
-        );
-    }
-    
-    // Conditional Rendering ?
-    const StatusBar = ({status}) => {
-        switch (status) {
-            case "normal":
-                return <StatusBarColor color={"#4caf50"} />;
-                break;
-            case "warning":
-                return <StatusBarColor color={"#ffeb3b"} />;
-                break;
-            case "critical":
-                return <StatusBarColor color={"#f44336"} />;
-                break;
-        }
-    };
-    // Render current tree node 
-    return (
-        <div>
-            <div class="container-style">
-                <div>
-                    <StatusBar status={node.status} />
-                    {node.position} <br />
-                    {node.location}
-                </div>
-            </div>
-
-            <OrgChart data={node.children} />
         </div>
     );
 };
