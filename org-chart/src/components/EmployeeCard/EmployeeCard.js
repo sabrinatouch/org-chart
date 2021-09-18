c91f738c548e46de98895fc2554edb51764eeee9
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 17:07:28 -0600

    Implemented tree functionality

--- org-chart/src/components/EmployeeCard/EmployeeCard.js
+++ org-chart/src/components/EmployeeCard/EmployeeCard.js
@@ -1,46 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './EmployeeCard.css';
+import OrgChart from '../OrgChart/OrgChart';
 
+// const EmployeeCard = ({node}) => {
+//     const [childVisible, setChildVisibility] = useState(false);
 
-export const EmployeeCard = ({employee}) => {
-    
-    const StatusBarColor = ({color}) => {
-        const statusBarStyle = {
-            height: 20,
-            width: '100%',
-            backgroundColor: color
-        }
-    
-        return (
-            <div style={statusBarStyle} />
-        );
-    }
-    
-    // Conditional Rendering ?
-    const StatusBar = ({status}) => {
-        switch (status) {
-            case "normal":
-                return <StatusBarColor color={"#4caf50"} />;
-                break;
-            case "warning":
-                return <StatusBarColor color={"#ffeb3b"} />;
-                break;
-            case "critical":
-                return <StatusBarColor color={"#f44336"} />;
-                break;
-        }
-    };
-    
-    return (
-        <div class="container-style">
-            <StatusBar status={employee.status} />
-            {/* <StatusBar color={} /> */}
-            <div>
-                {employee.position} <br />
-                {employee.location}
-            </div>
-        </div>
-    );
-};
+//     // Does the current tree node have children (true) or not (false)
+//     const hasChild = node.children ? true : false
 
-export default EmployeeCard;
+//     // const hasChild = () => {
+//     //     if (node.children) {
+//     //         return true;
+//     //     } else {
+//     //         return false;
+//     //     }
+//     // }
+
+//     const StatusBarColor = ({color}) => {
+//         const statusBarStyle = {
+//             height: 20,
+//             width: '100%',
+//             backgroundColor: color
+//         }
+    
+//         return (
+//             <div style={statusBarStyle} />
+//         );
+//     }
+    
+//     // Conditional Rendering ?
+//     const StatusBar = ({status}) => {
+//         switch (status) {
+//             case "normal":
+//                 return <StatusBarColor color={"#4caf50"} />;
+//                 break;
+//             case "warning":
+//                 return <StatusBarColor color={"#ffeb3b"} />;
+//                 break;
+//             case "critical":
+//                 return <StatusBarColor color={"#f44336"} />;
+//                 break;
+//         }
+//     };
+//     // Render current tree node 
+//     return (
+//         <div>
+//             <div class="container-style">
+//                 {
+//                     hasChild && 
+//                     <div>
+//                         <StatusBar status={node.status} />
+//                         {node.position} <br />
+//                         {node.location}
+//                     </div>
+//                 }
+//             </div>
+
+//             {
+//                 hasChild && 
+//                 <ul>
+//                     <OrgChart data={node.children} />
+//                 </ul>
+//             }
+//         </div>
+//     );
+// };
+
+// export default EmployeeCard;
