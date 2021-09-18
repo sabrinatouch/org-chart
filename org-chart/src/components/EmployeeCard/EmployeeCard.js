4a21de37824e541875a01293ce5ee0b13687b1ae
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 15:19:39 -0600

    Encapsulated Employee card code into EmployeeCard.js

--- org-chart/src/components/EmployeeCard/EmployeeCard.js
+++ org-chart/src/components/EmployeeCard/EmployeeCard.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
 import './EmployeeCard.css';
-import { data } from '../../data/data.js';
 
 
-class EmployeeCard extends Component {
-    render(){
-        return(
-            <div></div>
-        )
+export const EmployeeCard = ({employee}) => {
+    
+    const StatusBarColor = ({color}) => {
+        const statusBarStyle = {
+            height: 20,
+            width: '100%',
+            backgroundColor: color
+        }
+    
+        return (
+            <div style={statusBarStyle} />
+        );
     }
-}
+    
+    // Conditional Rendering ?
+    const StatusBar = ({status}) => {
+        switch (status) {
+            case "normal":
+                return <StatusBarColor color={"#4caf50"} />;
+                break;
+            case "warning":
+                return <StatusBarColor color={"#ffeb3b"} />;
+                break;
+            case "critical":
+                return <StatusBarColor color={"#f44336"} />;
+                break;
+        }
+    };
+    
+    return (
+        <div class="container-style">
+            <StatusBar status={employee.status} />
+            {/* <StatusBar color={} /> */}
+            <div>
+                {employee.position} <br />
+                {employee.location}
+            </div>
+        </div>
+    );
+};
 
 export default EmployeeCard;
--- org-chart/src/components/OrgChart/OrgChart.js
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -5,53 +5,13 @@ import './OrgChart.css';
 import EmployeeCard from '../EmployeeCard/EmployeeCard';
 import { data } from '../../data/data.js';
 
-const StatusBarColor = ({color}) => {
-    const statusBarStyle = {
-        height: 20,
-        width: '100%',
-        backgroundColor: color
-    }
-
-    return (
-        <div style={statusBarStyle} />
-    );
-}
-
-// Conditional Rendering ?
-const StatusBar = ({status}) => {
-    switch (status) {
-        case "normal":
-            return <StatusBarColor color={"#4caf50"} />;
-            break;
-        case "warning":
-            return <StatusBarColor color={"#ffeb3b"} />;
-            break;
-        case "critical":
-            return <StatusBarColor color={"#f44336"} />;
-            break;
-    }
-};
-
-const Employee = ({employee}) => {
-    return (
-        <div class="container-style">
-            <StatusBar status={employee.status} />
-            {/* <StatusBar color={} /> */}
-            <div>
-                {employee.position} <br />
-                {employee.location}
-            </div>
-        </div>
-    );
-};
-
 export const OrgChart = () => {
     return (
         <div>
             {data.map((employee, key) => {
                 return (
                     <div key={key}>
-                        <Employee employee={employee} />
+                        <EmployeeCard employee={employee} />
                     </div>
                 );
             })}
