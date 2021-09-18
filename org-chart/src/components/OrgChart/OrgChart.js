857e941e76b1a3a647335f255b7d7fc4c8ea9bb5
sabrinatouch <sabrinajindatouch@gmail.com>
2021-09-18 12:07:07 -0600

    Relocated JSON

--- org-chart/src/components/OrgChart/OrgChart.js
+++ org-chart/src/components/OrgChart/OrgChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import EmployeeCard from '../EmployeeCard/EmployeeCard';
+import Data from '../../data.json';
 
 class OrgChart extends Component {
     render() {
